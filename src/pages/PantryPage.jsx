7b92afd19fa3ded4c90fdab63cadce5ced8e7d5d
bDavis <BlakelyJDavis@gmail.com>
2026-07-13 00:50:39 -0400

    pantry-ui branch created and isAddFood modal implemented

--- src/pages/PantryPage.jsx
+++ src/pages/PantryPage.jsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
 import PantryList from "../features/pantry/PantryList";
 
 function PantryPage() {
+    const [isAddFoodOpen, setIsFoodOpen] = useState(false);
+
     return (
         <main>  
             <h2>My Pantry</h2>
+
+        <button type="button" onClick={() => setIsFoodOpen(true)}>
+            Add Food
+        </button>
+
+        {isAddFoodOpen && (
+            <section>
+                <h3>Add Pantry Item</h3>
+
+                <button type="button" onClick={() => setIsFoodOpen(false)}>
+                    Cancel
+                </button>
+            </section>
+        )}
+
             <PantryList />
         </main>
     );
