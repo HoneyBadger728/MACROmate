0fc37b96eabaaaf18e096fc34f66b2795ff5c427
bDavis <BlakelyJDavis@gmail.com>
2026-08-17 12:11:01 -0400

    applied styling to Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,21 +5,21 @@ function Navigation() {
     return (
         <nav className="bottom-nav" aria-label="Primary navigation">
             <NavLink 
-                className="bottom-nav_link bottom-nav__link--pantry"  
+                className="bottom-nav__link bottom-nav__link--pantry"  
                 to="/pantry"
             >
                 My Pantry
             </NavLink>
             
             <NavLink 
-                className="bottom-nav_link bottom-nav__link--home" 
+                className="bottom-nav__link bottom-nav__link--home" 
                 to="/" end
             >
                 MACROmate
             </NavLink>
             
             <NavLink 
-                className="bottom-nav_link bottom-nav__link--meals" 
+                className="bottom-nav__link bottom-nav__link--meals" 
                 to="/meals"
             >
                 Today's Meals
